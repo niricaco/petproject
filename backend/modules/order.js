4060711d768c77e513d134fdc3b5ce11ebf8fb18
Kubi <KubiHUN@users.noreply.github.com>
2022-05-19 12:50:38 +0200

    order model ready

---
+++ backend/modules/order.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  orderNumber: {
+    type: String,
+    required: true,
+  },
+  orderedBy: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  confirmed: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isUnderCollection: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isCollected: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  work: {
+    type: String,
+    required: true,
+  },
+  specialization: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  detials: [],
+  orderDate: {
+    type: Date,
+    required: true,
+  },
+  orderTime: {
+    type: Date,
+    required: true,
+  },
+});
+
+const OrderEntity = mongoose.model("orderEntity", orderSchema);
+
+module.exports = OrderEntity;
