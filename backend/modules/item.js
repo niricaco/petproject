7e8a4508eb4dd5b56bd00772b823c65d25bd842a
niricaco <64769591+niricaco@users.noreply.github.com>
2022-05-20 16:16:11 +0200

    item.js push

--- backend/modules/item.js
+++ backend/modules/item.js
@@ -1,12 +1,54 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  orderNumber: {
+const itemSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true,
   },
+  partNo: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: null,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: null,
+  },
+  discount: {
+    type: Number,
+    required: true,
+    default: null,
+  },
+  currency: {
+    type: String,
+    required: true,
+    default: "HUF",
+  },
+  tags: {
+    type: Array,
+    required: false,
+  },
+  specializations: {
+    type: Array,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  note: {
+    type: String,
+    required: false,
+  },
+  detials: [],
 });
 
-const OrderEntity = mongoose.model("orderEntity", orderSchema);
+const ItemEntity = mongoose.model("itemEntity", itemSchema);
 
-module.exports = OrderEntity;
+module.exports = ItemEntity;
